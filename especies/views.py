c965d15d628556202d0944dfca38df9b5ddc54ff
HeroiDerrotado <bnmbruninnho@gmail.com>
2023-07-18 21:03:07 -0300

    colocando a base das views e urls e pastas globais

--- especies/views.py
+++ especies/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def especies_views(request):
+    return render(request,"")
--- pesca/views.py
+++ pesca/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def pesca_views(request):
+    return render(request,"")
