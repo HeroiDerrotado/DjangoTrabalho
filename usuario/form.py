aa0b03649f0bb6ca7f5d6a2f79e942d3b86cd9db
HeroiDerrotado <bnmbruninnho@gmail.com>
2023-07-24 20:16:26 -0300

    colocando o cadastro e colocando admin

--- usuario/form.py
+++ usuario/form.py
@@ -18,7 +18,57 @@ class LoginForms(forms.Form):
         max_length= 70,
         widget= forms.PasswordInput(
             attrs = {
-                'placeholder' : "digite sua senha"
+                'placeholder' : "Digite sua senha"
             }
         )
     )
+
+class CadastroForms(forms.Form):
+
+    nome_completo = forms.CharField(
+        label = 'Seu cadastro',
+        required = True,
+        max_length = 100,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder' : "Digite seu nome"
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        label = "email",
+        required = True,
+        max_length = True,
+        widget = forms.TextInput(
+            attrs = {
+                'placeholder' : "Digite seu nome"
+            }
+        )
+    )
+
+    senha_cadastro1 = forms.CharField(
+        label='senha1',
+        required= True,
+        max_length= 70,
+        widget= forms.PasswordInput(
+            attrs = {
+                'placeholder' : "Digite sua senha"
+            }
+        )
+    )
+
+
+    senha_cadastro2 = forms.CharField(
+        label='senha2',
+        required= True,
+        max_length= 70,
+        widget= forms.PasswordInput(
+            attrs = {
+                'placeholder' : "Digite sua senha novamente"
+            }
+        )
+    )
+
+
+
--- usuario/views.py
+++ usuario/views.py
@@ -1,11 +1,60 @@
-from django.shortcuts import render
-from usuario.form import LoginForms 
+
+from usuario.form import LoginForms, CadastroForms
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
 def login_views(request):
     formulario= LoginForms
+
+    if formulario.is_valid():
+
+        nome=formulario['nome_login'].value()
+        senha=formulario['senha'].value()
+
+        usuario = auth.authenticate(
+            request,
+            usename= nome,
+            password = senha
+        )
+
+        if usuario is not None:
+            auth.login(request,usuario)
+            return redirect('cadastro:index')
+        else:
+            return redirect('usuario:login')
+
+
     return render(request,"usuario/paginas/usuario.html",context={'formulario':formulario})
 
 def cadastro_views(request):
-    return render(request,"usuario/paginas/cadastro.html")
+    formulario = CadastroForms()
+
+    if request.method == "POST":
+        formulario = CadastroForms(request.POST)
+    
+        if formulario.is_valid():
+            if formulario['senha1'].value() == formulario['senha2'].value():
+                return redirect('usuario:cadastro')
+    
+            nome=formulario['nome_cadastro'].value()
+            email=formulario['email_cadastro'].value()
+            senha=formulario['senha_1'].value()
+            senha= formulario['senha_2'].value()
+
+        if User.objects.filter(username=nome).exists():
+            return redirect('usuario:cadastro')
+
+        novo_usuario = User.objects.create_user(
+
+            username = nome,
+            email = email,
+            password = senha
+        )
+
+        novo_usuario.save()
+        return redirect('usuario:login')
+    return render(request,"usuario/paginas/cadastro.html",context ={'formulario':formulario})
+
