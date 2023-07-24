08af034efe6bc5dd4a6f5a72bf597bd85f1b7239
HeroiDerrotado <bnmbruninnho@gmail.com>
2023-07-24 19:05:07 -0300

    resolvendo erros do site

--- vidamarinha/views.py
+++ vidamarinha/views.py
@@ -4,7 +4,7 @@ from vidamarinha.models import Imagem
 # Create your views here.
 
 def vidamarinha_views(request):
-    return render(request,"vidamarinha/paginas/vidamarinha_head.html")
+    return render(request,"vidamarinha/paginas/vidamarinha_body.html")
 
 def busca_views(request):
 
