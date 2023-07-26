edbf252a0d53e2b1a7edaffbc5915754fabfcc99
patischmitt <patischmitt@gmail.com>
2023-07-25 21:40:06 -0300

    inclusão dos arquivos do site html

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Menu(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ('CATEGORIA','categoria'),
+        ('CAT','cat'),
+    ]
+    nome = models.CharField(max_length=100,null = False, blank = False )
+    legenda = models.CharField(max_length=100,null =False,blank=False)
+    categoria = models.CharField(max_length=100, choices= OPCOES_CATEGORIA, default= "")
+    descricao = models.TextField(null=False, blank=False)
+    descrisao = models.TextField(null = False,blank= False)
+    foto = models.ImageField(upload_to='imagem/%Y/%m/%d/',blank=True)
+    publicada = models.BooleanField(default=False)
+    data = models.DateTimeField(default=datetime.now, blank=False)
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from menu.models import Menu
 
 # Create your views here.
 
 def menu_views(request):
     return render(request,'menu/paginas/menu.html')
+
+def views_busca(request):
+
+    imagens = Menu.objects.all()
+    print(request)
+
+    if 'buscando' in request.GET:
+        nome = request.GET['buscando']
+        if nome:
+            imagens = imagens.filter(nome__icontains=nome)
+
+    return render(request, 'menu/paginas/busca.html', context ={'imagens' : imagens})
